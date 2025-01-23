9ee053dc5ac6575258206ffc6fc22d0120143ced
Indradeep Mandal <indradeepmandal18@gmail.com>
2025-01-23 20:27:27 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "https://community-nu-five.vercel.app",
+     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
