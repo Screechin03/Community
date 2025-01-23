ea958707f26183f4e63b3122ccece4c586aad765
Indradeep Mandal <indradeepmandal18@gmail.com>
2025-01-23 20:54:33 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://community-533m.vercel.app/api",
+    baseURL: "https://community-f9zq.vercel.app/api",
     withCredentials: true,
 })
