27ecec10ce28dfea8a7b744951ed478a0bd00deb
Indradeep Mandal <indradeepmandal18@gmail.com>
2025-01-23 20:57:28 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://community-71rn.vercel.app",
+    origin: "https://community-phnw.vercel.app/",
     credentials: true,
 }));
 
